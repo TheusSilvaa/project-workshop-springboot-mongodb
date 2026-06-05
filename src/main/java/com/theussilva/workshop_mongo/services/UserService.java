d26b33426487340ba45723a43a3bc119fb707458
Matheus Silva <msilvarodrigues011@gmail.com>
2026-06-05 17:18:35 -0300

    Obtendo um usuario por id

--- src/main/java/com/theussilva/workshop_mongo/services/UserService.java
+++ src/main/java/com/theussilva/workshop_mongo/services/UserService.java
@@ -2,10 +2,13 @@ package com.theussilva.workshop_mongo.services;
 
 import com.theussilva.workshop_mongo.domain.User;
 import com.theussilva.workshop_mongo.repository.UserRepository;
+import com.theussilva.workshop_mongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -16,4 +19,10 @@ public class UserService {
     public List<User> findAll() {
         return repo.findAll();
     }
-}
+
+    public User findById(String id) {
+        Optional<User> user = repo.findById(id);
+        return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+        }
+    }
+
