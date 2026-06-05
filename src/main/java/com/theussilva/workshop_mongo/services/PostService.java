780cf96945c54084f07c0cf5af8b208cb430b197
Matheus Silva <msilvarodrigues011@gmail.com>
2026-06-05 19:53:44 -0300

    Obtendo um post por id

---
+++ src/main/java/com/theussilva/workshop_mongo/services/PostService.java
@@ -0,0 +1,25 @@
+package com.theussilva.workshop_mongo.services;
+
+import com.theussilva.workshop_mongo.domain.Post;
+import com.theussilva.workshop_mongo.domain.User;
+import com.theussilva.workshop_mongo.dto.UserDTO;
+import com.theussilva.workshop_mongo.repository.PostRepository;
+import com.theussilva.workshop_mongo.repository.UserRepository;
+import com.theussilva.workshop_mongo.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+    public Post findById(String id) {
+        Optional<Post> user = repo.findById(id);
+        return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+}
