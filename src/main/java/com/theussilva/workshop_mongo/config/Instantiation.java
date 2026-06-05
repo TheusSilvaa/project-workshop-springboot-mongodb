98d822ac5fde3f870163076a35b7507452696747
Matheus Silva <msilvarodrigues011@gmail.com>
2026-06-05 19:35:30 -0300

    Referenciando os posts do usuario

--- src/main/java/com/theussilva/workshop_mongo/config/Instantiation.java
+++ src/main/java/com/theussilva/workshop_mongo/config/Instantiation.java
@@ -41,5 +41,8 @@ public class Instantiation implements CommandLineRunner {
         Post post2 = new Post(null, sdf.parse("20/03/2026"), "Bom dia", "Acordei com o pé direito hoje, estou muito feliz!", new AuthorDTO(maria));
 
         postRepository.saveAll(Arrays.asList(post1, post2));
+
+        maria.getPosts().addAll(Arrays.asList(post1, post2));
+        userRepository.save(maria);
     }
 }
--- src/main/java/com/theussilva/workshop_mongo/domain/User.java
+++ src/main/java/com/theussilva/workshop_mongo/domain/User.java
@@ -1,9 +1,12 @@
 package com.theussilva.workshop_mongo.domain;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Document(collection = "user")
@@ -14,6 +17,9 @@ public class User implements Serializable {
     private String name;
     private String email;
 
+    @DBRef(lazy = true)
+    private List<Post> posts = new ArrayList<>();
+
     public User() {
     }
 
@@ -22,6 +28,7 @@ public class User implements Serializable {
         this.name = name;
         this.email = email;
     }
+
 
     public String getId() {
         return id;
@@ -47,6 +54,13 @@ public class User implements Serializable {
         this.email = email;
     }
 
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
+    }
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
