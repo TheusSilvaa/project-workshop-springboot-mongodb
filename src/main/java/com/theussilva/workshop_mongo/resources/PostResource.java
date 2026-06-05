0d9936b6e58bb30c65644f07cadfcb7389099ec6
Matheus Silva <msilvarodrigues011@gmail.com>
2026-06-05 20:35:08 -0300

    Consulta simples com query methods

--- src/main/java/com/theussilva/workshop_mongo/resources/PostResource.java
+++ src/main/java/com/theussilva/workshop_mongo/resources/PostResource.java
@@ -3,6 +3,7 @@ package com.theussilva.workshop_mongo.resources;
 import com.theussilva.workshop_mongo.domain.Post;
 import com.theussilva.workshop_mongo.domain.User;
 import com.theussilva.workshop_mongo.dto.UserDTO;
+import com.theussilva.workshop_mongo.resources.util.URL;
 import com.theussilva.workshop_mongo.services.PostService;
 import com.theussilva.workshop_mongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,6 +26,13 @@ public class PostResource {
     public ResponseEntity<Post> findById(@PathVariable String id) {
         Post obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+        text = URL.decodeParam(text);
+        List<Post> list = service.findByTitle(text);
+        return ResponseEntity.ok().body(list);
     }
 
 }
--- src/main/java/com/theussilva/workshop_mongo/services/PostService.java
+++ src/main/java/com/theussilva/workshop_mongo/services/PostService.java
@@ -22,4 +22,8 @@ public class PostService {
         Optional<Post> user = repo.findById(id);
         return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
+
+    public List<Post> findByTitle(String text) {
+        return repo.findByTitleContainingIgnoreCase(text);
+    }
 }
